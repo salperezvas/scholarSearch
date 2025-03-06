3b55ec9c8dd7649df3a213a248aa96170a9e55c1
salperezvas <113035201+salperezvas@users.noreply.github.com>
2025-03-05 23:25:18 -0500

    created signUp.js

---
+++ client/js/signUp.js
@@ -0,0 +1,37 @@
+$('#data-submit').click(function(event) {
+    event.preventDefault();
+
+    var username = $('#username').val();
+    var password = $('#password').val();
+
+    var jsonString = {
+        username: username,
+        password: password
+    };
+
+    $.ajax({
+        url: "http://localhost:777/login",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(jsonString),
+        success: function(response) {
+            if (response.msg === "Login successful") {
+                alert("Welcome, " + username);
+                window.location.href = "studentDB"; 
+            } else if (response.msg === "Please enter username and password") {
+                alert("Please enter username and password");
+            } else if (response.msg === "Database error") {
+                alert("Database error");
+            } else if (response.msg === "Invalid username or password") {
+                alert("Invalid username or password");
+            } else {
+                alert("Error: " + response.msg);
+            }
+        },
+        error: function(err) {
+            alert("An error occurred. Please try again. lmao it's not working");
+        }
+    });
+
+    return false;
+});

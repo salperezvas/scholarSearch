cbb70bf201be41b65809e30f9a317a2b2aa0d583
salperezvas <113035201+salperezvas@users.noreply.github.com>
2025-04-09 16:07:36 -0400

    Fux bugs

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-
 const session = require('express-session');
 
 app.use(cors());
